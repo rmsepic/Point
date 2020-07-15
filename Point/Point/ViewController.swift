c8a647e058128b24ff50af54314fe955a5092ac0
rmsepic <rmsepic@ucdavis.edu>
2020-07-15 14:36:22 -0700

    Initial commit

---
+++ Point/Point/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Point
+//
+//  Created by Ryland Sepic on 7/15/20.
+//  Copyright © 2020 Ryland Sepic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
