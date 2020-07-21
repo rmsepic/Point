0254b79f98f50d0ee9816e95192ec14ad41ab973
rmsepic <rmsepic@ucdavis.edu>
2020-07-21 13:10:47 -0700

    Sharing enabled.

--- Point/Point/SurveyViewController.swift
+++ Point/Point/SurveyViewController.swift
@@ -7,18 +7,17 @@
 //
 
 import CoreLocation
+import MessageUI
 import UIKit
 
 
-
-class SurveyViewController: UIViewController {
+class SurveyViewController: UIViewController, MFMailComposeViewControllerDelegate {
     let location_manager = CLLocationManager()  // NEEDS to be declared globally so the permissions notification shows properly
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var curr_location: CLLocation!
-        
         self.location_manager.requestWhenInUseAuthorization()
         
         if(CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse) {
@@ -35,8 +34,24 @@ class SurveyViewController: UIViewController {
         createFile()
     }
     
+    /// Export the information from the CSV file
+    @IBAction func export(_ sender: Any) {
+        let file = getFilePath()
+        let activityViewController : UIActivityViewController = UIActivityViewController(activityItems: [file], applicationActivities: nil)
+
+        activityViewController.excludedActivityTypes = [
+            UIActivity.ActivityType.assignToContact,
+            UIActivity.ActivityType.saveToCameraRoll,
+            UIActivity.ActivityType.addToReadingList,
+            UIActivity.ActivityType.postToFlickr,
+            UIActivity.ActivityType.postToVimeo,
+        ]
+
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
     func createFile() {
-        let file = "file2.csv"
+        let file = "file.csv"
         let text = "some text"
         if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
             let file_url = dir.appendingPathComponent(file)
@@ -50,8 +65,8 @@ class SurveyViewController: UIViewController {
 
     }
     
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+    /// Function returns the file path for the stored CSV file
+    func getFilePath() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
     }
 }
